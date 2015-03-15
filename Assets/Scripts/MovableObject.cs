78613bd8b711892a0ba6d0d648b0045ac456874f
MaksTsk <muriginm@gmail.com>
2015-03-15 19:03:51 +0600

    Add Materials and MovableObject Script

---
+++ Assets/Scripts/MovableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshCollider))]
+public class MovableObject : MonoBehaviour {
+
+    private Vector3 _screenPoint;
+    private Vector3 _offset;
+
+    private void OnMouseDown()
+    {
+        _screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        _offset = gameObject.transform.position -
+                 Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
+
+    }
+
+    private void OnMouseDrag()
+    {
+        var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+
+        var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + _offset;
+
+        transform.position = curPosition;
+    }
+}
